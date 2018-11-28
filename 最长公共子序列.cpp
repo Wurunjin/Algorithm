2973d3fd9607b2f800ea3bbf305dac488af2f44a
吴润瑾 <37664235+Wurunjin@users.noreply.github.com>
2018-11-28 16:46:56 +0800

    Update and rename 最长公共子序列O(n^2).cpp to 最长公共子序列.cpp

--- 最长公共子序列.cpp
+++ 最长公共子序列.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int v[100000];
 int dp[100001]={0};
-int LIS(int n){
+int LIS_01(int n){
     for(int i=0;i<n;i++)cin>>v[i];
     for(int i=0;i<n;i++){
         dp[i]=1;
@@ -14,10 +14,26 @@ int LIS(int n){
     }
     return *max_element(dp,dp+n);
 }
+int LIS_02(int n){
+    int len=1;
+    for(int i=0;i<n;i++)cin>>v[i];
+    dp[0]=v[0];
+    for(int i=1;i<n;i++)
+    {
+        if(v[i]>dp[len]){
+            dp[++len]=v[i];
+        }else{
+            int j=lower_bound(dp+1,dp+len+1,v[i])-dp;
+                    dp[j]=v[i];
+        }
+    }
+    return len;
+}
 int main() {
     int n;
     cin>>n;
-    cout<<LIS(n)<<endl;
+    cout<<LIS_01(n)<<endl;
+    cout<<LIS_o2(n)<<endl;
 }
 //我们可以通过二分查找中改变“上确界”和“下确界”，以及符号（“<”和“<=”或“>”、“>=”等），求出最长不下降、不上升、严格下降子序列等问题。
 
