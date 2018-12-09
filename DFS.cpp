d100da2d6bb2fdf991b231c166dde0df37920008
吴润瑾 <37664235+Wurunjin@users.noreply.github.com>
2018-12-09 11:20:22 +0800

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,37 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a,b,c,d,m,n,mn,s,mp[22][22],v[22][22]={0};
-int ans=0;
-int net[4][2]={ {0,1}, {1,0}, {0,-1},{-1,0}};
-void dfs(int x,int y,int step){
-    int tx,ty;
+int m,n,sx,sy,mn,mp[22][22],c,d,s;
+void prt(){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            printf("%2d",mp[i][j]);
+        }cout<<endl;
+    }cout<<endl;
+}
+void dfs(int x,int y){
+    if(!(x>=0&&x<m&&y>=0&&y<n&&mp[x][y]==0)) return ;
     if(x==c&&y==d){
-        ans++;
-        if(step<mn)mn=step;
-        //cout<<step<<endl;
-        return;
+        if(s<mn){mn=s;}return ;
     }
-    for(int i=0;i<4;i++){
-        tx=x+net[i][0];
-        ty=y+net[i][1];
-        if(!(x>=0&&x<m&&y>=0&&y<n))return;
-        if(mp[tx][ty]==0&&v[tx][ty]==0){
-            v[tx][ty]=1;
-            dfs(tx,ty,step+1);
-            v[tx][ty]=0;
-        }
+    s++;
+    mp[x][y]=2;
+    dfs(x+1,y);
+    dfs(x-1,y);
+    dfs(x,y+1);
+    dfs(x,y-1);
+    if(mp[x][y]==2){
+        s--;mp[x][y]=0;
     }
 }
 int main(){
-    while(cin>>m>>n){
-        s=0;mn=999999;
+    while(cin>>m>>n&&m&&n){
+        mn=999999;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-                cin>>mp[i][j];
-        }cin>>a>>b>>c>>d;a--;b--;c--;d--;
-        dfs(a,b,0);
-        //cout<<"ans="<<ans<<endl;
-        (mn==999999)?cout<<"No Way!"<<endl:cout<<mn<<endl;
+            for(int j=0;j<n;j++)cin>>mp[i][j];
+        }
+        cin>>sx>>sy>>c>>d;
+        sx--;sy--;c--;d--;
+        dfs(sx,sy);
+        if(mn==999999){
+            cout<<"No Way!"<<endl;
+        }else{
+            cout<<mn<<endl;
+        }
     }
-    return 0;
 }
